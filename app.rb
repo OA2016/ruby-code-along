05f741188accc293c575d4f357541a5254eda79a
oa2016 <omar.amezcua.simontech.2016@gmail.com>
2016-03-01 23:17:10 +0000

    first commit, yo.

---
+++ app.rb
@@ -0,0 +1,16 @@
+    require_relative 'config/environment'
+
+    class App < Sinatra::Base 
+    
+    get '/' do 
+    erb :index
+end 
+
+    post "/" do 
+        @user = {
+     name: "#{params["fname"]}  #{params["lname"]}",
+        profile_pic_url: "#{params["picurl"]}"
+     erb :dashboard
+    end 
+        
+    end 
